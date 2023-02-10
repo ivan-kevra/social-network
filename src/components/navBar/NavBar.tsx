510f8da5abe21a956b378df2634c5cff888444f4
Ivan Kevra <78702646+ivankevra@users.noreply.github.com>
2023-02-10 19:39:49 +0300

    20th lesson

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import style from './NavBar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href='/profile'>Profile</a></div>
-            <div className={`${style.item} ${style.active}`}><a href='/dialogs'>Messages</a></div>
-            <div className={style.item}><a>News</a></div>
-            <div className={style.item}><a>Music</a></div>
-            <div className={style.item}><a>Settings</a></div>
+            <div className={style.item}>
+                <NavLink to={'/profile'}
+                         className={({isActive}) => isActive ? style.active : undefined}>Profile</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={'/dialogs'}
+                         className={({isActive}) => isActive ? style.active : undefined}>Messages</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={'/news'}
+                         className={({isActive}) => isActive ? style.active : undefined}>News</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={'/music'}
+                         className={({isActive}) => isActive ? style.active : undefined}>Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to={'/settings'}
+                         className={({isActive}) => isActive ? style.active : undefined}>Settings</NavLink>
+            </div>
         </nav>
     )
 }
