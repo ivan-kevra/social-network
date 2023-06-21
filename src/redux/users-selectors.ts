928d3b7ee5ece7774e50233898cc90a9b997d10a
ivan kevra <vanyakevra@gmail.com>
2023-06-21 11:23:14 +0300

    83: reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -3,18 +3,22 @@ import {AppRootStateType} from "./redux-store";
 export const getUsers = (state: AppRootStateType) => {
     return state.usersPage
 }
+// export const getUsers = createSelector(getUsersSelector, (users) => {
+//     return users.filter((user: UserType) => true)
+// })
 export const getPageSize = (state: AppRootStateType) => {
     return state.usersPage.pageSize
 }
 export const getTotalUserCount = (state: AppRootStateType) => {
     return state.usersPage.totalUsersCount
 }
-export const getCurrentPage = (state: AppRootStateType) => {
-    return state.usersPage.currentPage
-}
 export const getIsFetching = (state: AppRootStateType) => {
     return state.usersPage.isFetching
 }
+export const getCurrentPage = (state: AppRootStateType) => {
+    return state.usersPage.currentPage
+}
+
 export const getFollowingInProgress = (state: AppRootStateType) => {
     return state.usersPage.followingInProgress
 }
